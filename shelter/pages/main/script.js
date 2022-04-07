f94b825f179539b530e841d292fd0dfa075d5755
Roman Troshin <roman.troshin98@gmail.com>
2022-04-07 12:25:32 +0300

    feat: add link on buttons in not-only and our-friends sections

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,8 +1,13 @@
 import menuSwitchActive from "./js/menuActive.js";
 import { burgerMenuOpen, burgerMenuClose } from "./js/burgerMenu.js";
+import { linkBtns } from "./js/variables.js";
 
 window.onload = () => {
   menuSwitchActive;
   burgerMenuOpen;
   burgerMenuClose;
 }
+
+linkBtns.forEach(elem => elem.addEventListener('click', function () {
+  location.href = "../../pages/pets/index.html";
+}))
