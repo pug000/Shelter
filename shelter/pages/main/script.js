c6e5bff0b4ddd2e799b0de8558930e9244626872
Roman Troshin <roman.troshin98@gmail.com>
2022-04-11 15:51:44 +0300

    fix: link buttons

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,6 +1,8 @@
 import menuSwitchActive from "./js/menuActive.js";
 import { burgerMenuOpen, burgerMenuClose } from "./js/burgerMenu.js";
-
+import { linkBtns } from "./js/variables.js";
+// import { slider } from './js/slider.js';
+import pets from './js/pets.json' assert {type: 'json'};
 
 window.onload = () => {
   menuSwitchActive;
@@ -11,5 +13,4 @@ window.onload = () => {
 linkBtns.forEach(elem => elem.addEventListener('click', function () {
   location.href = "../../pages/pets/index.html";
 }))
-
 
