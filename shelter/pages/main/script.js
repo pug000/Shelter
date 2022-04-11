fbdcd432f8029c0ec1e1e38c1f62f776d5795871
Roman Troshin <roman.troshin98@gmail.com>
2022-04-11 21:52:01 +0300

    refactor: change padding

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,8 +1,6 @@
 import menuSwitchActive from "./js/menuActive.js";
 import { burgerMenuOpen, burgerMenuClose } from "./js/burgerMenu.js";
 import { linkBtns } from "./js/variables.js";
-// import { slider } from './js/slider.js';
-import pets from './js/pets.json' assert {type: 'json'};
 
 window.onload = () => {
   menuSwitchActive;
@@ -12,5 +10,4 @@ window.onload = () => {
 
 linkBtns.forEach(elem => elem.addEventListener('click', function () {
   location.href = "../../pages/pets/index.html";
-}))
-
+}));
